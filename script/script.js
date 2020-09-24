48f0dfb374580445075d8fd2a5cee9977b0ed782
Giancarlo Zuliani <slaigox@gmail.com>
2020-09-24 23:04:46 +0200

    added filter on footer hover

--- script/script.js
+++ script/script.js
@@ -12,18 +12,19 @@ var x = true
 magnifier.addEventListener('click',function(){
 
   if(x){
-    textInputNavBar.style.width = "0px"
-    textInputNavBar.style.opacity = "0"
-  }
-  else {
     textInputNavBar.style.width = "200px"
     textInputNavBar.style.opacity ="1"
+  }
+  else {
+    textInputNavBar.style.width = "0px"
+    textInputNavBar.style.opacity = "0"
   };
-  x!=x
+  x=!x
 })
 
 
 //widget image change on click and chat div show
+
 
 const chatcircle = document.querySelector('#cross')
 const chatbubble = document.querySelector('#bubble')

f7e145465a5ac2398a400a0488cb33af154df404
Giancarlo Zuliani <slaigox@gmail.com>
2020-09-24 19:53:39 +0200

    Added script that show text input on magnifier click on nav bar

---
+++ script/script.js
@@ -0,0 +1,24 @@
+
+
+//show and hide text input on header navbar
+
+
+const magnifier = document.querySelector('#magnifier');
+const textInputNavBar = document.querySelector('.hiddenSearch')
+
+var x = true
+
+
+magnifier.addEventListener('click',function(){
+
+  if(x){
+    textInputNavBar.style.width = "200px"
+    textInputNavBar.style.opacity ="1"
+  }
+  else {
+    textInputNavBar.style.width = "0px"
+    textInputNavBar.style.opacity = "0"
+  };
+
+  x=!x
+})

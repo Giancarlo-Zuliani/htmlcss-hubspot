5f0e03fb5604c3809c3ff5872910a03bf2255a82
Giancarlo Zuliani <slaigox@gmail.com>
2020-09-24 20:46:02 +0200

    HubBot onclick

--- script/script.js
+++ script/script.js
@@ -12,12 +12,38 @@ var x = true
 magnifier.addEventListener('click',function(){
 
   if(x){
-    textInputNavBar.style.width = "200px"
-    textInputNavBar.style.opacity ="1"
-  }
-  else {
     textInputNavBar.style.width = "0px"
     textInputNavBar.style.opacity = "0"
+  }
+  else {
+    textInputNavBar.style.width = "200px"
+    textInputNavBar.style.opacity ="1"
   };
-  x=!x
+  x!=x
+})
+
+
+//widget image change on click and chat div show
+
+const chatcircle = document.querySelector('#cross')
+const chatbubble = document.querySelector('#bubble')
+const widget = document.querySelector('.widget')
+
+var chatrun = false
+
+widget.addEventListener('click',function(){
+
+  if (chatrun) {
+    chatcircle.classList.toggle('hidden')
+    chatbubble.classList.toggle('show')
+    chatcircle.classList.toggle('show')
+    chatbubble.classList.toggle('hidden')
+  }
+  else{
+    chatcircle.classList.toggle('show')
+    chatbubble.classList.toggle('hidden')
+    chatcircle.classList.toggle('hidden')
+    chatbubble.classList.toggle('show')
+  }
+  chatrun!=chatrun
 })
